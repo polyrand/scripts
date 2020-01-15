592ed6a13c54f7b7bebf4f12fa3c9fbd3629eb6a
rickaa <37962604+d0l-phin@users.noreply.github.com>
2020-01-15 19:10:40 +0100

    build lab only once, add bandit and jupyterlab-git

--- condacreate.sh
+++ condacreate.sh
@@ -16,25 +16,30 @@ echo "Using Python version: $VERSION"
 
 conda create --name "$NAME" python="$VERSION"
 
-conda install --name "$NAME" -c conda-forge numpy pandas ipython black flake8 matplotlib pycodestyle pydocstyle flake8-bugbear more-itertools jupyterlab mypy
+# basic libs
+conda install --name "$NAME" -c conda-forge numpy pandas ipython black flake8 matplotlib pycodestyle pydocstyle flake8-bugbear more-itertools jupyterlab mypy bandit
 
-conda run -n "$NAME" jupyter labextension install @jupyterlab/toc @jupyterlab/celltags @jupyter-widgets/jupyterlab-manager @ryantam626/jupyterlab_code_formatter
+# jupyter lab extensions
+conda run -n "$NAME" jupyter labextension install @jupyterlab/toc --no-build
+conda run -n "$NAME" jupyter labextension install @jupyterlab/celltags --no-build
+conda run -n "$NAME" jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
+conda run -n "$NAME" jupyter labextension install @ryantam626/jupyterlab_code_formatter --no-build
+conda install --name "$NAME" -c conda-forge jupyterlab-git
 
 conda install --name "$NAME" -c conda-forge jupyterlab_code_formatter
 
 conda run -n "$NAME" jupyter serverextension enable --py jupyterlab_code_formatter
 
-# youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --no-playlist -o 'dl.%(ext)s' $ID
+conda run -n "$NAME" jupyter lab build
 
-
-
-if [ $3 == 'alias' ]; then
+if [[ $3 == 'alias' ]]; then
     echo "alias $NAME='conda activate $NAME'" >> ~/dotfiles/.condalias
 # else
 #     ffmpeg -ss "$START" -t "$LENGTH" -i dl.mp4 -an -vf "setpts=(1/"$SPEED")*PTS" "$NAME".mp4
 fi
 
 
+# youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --no-playlist -o 'dl.%(ext)s' $ID
 # echo " ===> Removing files..."
 # rm dl.mp4
 # echo " ===> Done! "
