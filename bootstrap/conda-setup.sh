e16fb99d5ea34a7b22abfe145d539a97e69e3b4f
rickaa <37962604+d0l-phin@users.noreply.github.com>
2020-11-04 19:24:17 +0100

    📝 update

---
+++ bootstrap/conda-setup.sh
@@ -0,0 +1,35 @@
+set -eou pipefail
+
+cd
+
+case "$OSTYPE" in
+  darwin*)  DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh; ;;
+  linux*)   DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh; ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
+
+case "$SHELL" in
+  /bin/zsh*)   SHELL_NAME=zsh; ;;
+  /bin/bash*)  SHELL_NAME=bash ;;
+  /usr/local/bin/fish*) SHELL_NAME=fish ;;
+  *)        echo "unknown: $SHELL" ;;
+esac
+
+cat << EOF > .condarc
+channels:
+  - fastai
+  - pytorch
+  - defaults
+channel_priority: strict
+EOF
+
+wget -q $DOWNLOAD
+bash Miniconda3-latest*.sh -b
+~/miniconda3/bin/conda init $SHELL_NAME
+rm Miniconda3-latest*.sh
+
+. ~/.bashrc
+
+conda install -yq mamba
+
+cd -
